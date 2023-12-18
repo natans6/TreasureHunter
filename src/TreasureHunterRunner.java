6b920d2bb0899e6fef192cb10bee49a0be4ebb07
BT_1N3_17 <sample@bths.edu>
2023-12-18 08:35:54 -0500

    Added print statement

--- src/TreasureHunterRunner.java
+++ src/TreasureHunterRunner.java
@@ -1,5 +1,6 @@
 public class TreasureHunterRunner {
     public static void main(String[] args) {
+        System.out.println("First developer is Natan");
         TreasureHunter game = new TreasureHunter();
         game.play();
     }
